b38394330eb43b93b059c40ef6e693a381f4df21
Ihor <93588430+IhorDovhanych@users.noreply.github.com>
2023-06-29 18:37:04 +0300

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,6 +10,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+app.use(cors({
+    origin: '*'
+}));
 
 
 db.once('open', () => {
